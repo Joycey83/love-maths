14fe5c526276716a8e46ea900e187ea7f6087fdc
Joycey <Jcheung8385@gmail.com>
2021-11-08 10:53:47 +0000

    Add event listners for document loading

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,20 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons) {
+         button.addEventListener("click", function() {
+             if (this.getAttribute("data-type") === "submit") {
+                 alert("you clicked submit!");
+             } else {
+                 let gameType = this.getAttribute("data-type");
+                 alert(`you clicked ${gameType}`);
+             }
+         })
+    }
+        
+    
+})
+
 function runGame() {
 
 }
