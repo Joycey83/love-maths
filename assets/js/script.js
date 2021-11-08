fa819f9d913b9557af9e5f616e1c89d9b6208fad
Joycey <Jcheung8385@gmail.com>
2021-11-08 11:57:43 +0000

    Display addition questions to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,17 +7,24 @@ document.addEventListener("DOMContentLoaded", function() {
                  alert("you clicked submit!");
              } else {
                  let gameType = this.getAttribute("data-type");
-                 alert(`you clicked ${gameType}`);
+                 runGame(gameType);
              }
-         })
+         });
     }
-        
+    runGame("addition");
     
-})
-
-function runGame() {
+});
+//created 2 random numbers between 1 & 25
+function runGame(gameType) {
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer() {
@@ -36,8 +43,11 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
+function displayAdditionQuestion(operand1, operand2) {
+    
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
 }
 
 function displaySubtractQuestion() {
